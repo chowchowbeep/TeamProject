5ef1b0697106bd7a25f7ad6b8a11c267ff4ae1a4
User <User@YD03-15>
2020-02-11 15:08:22 +0900

    src안에 dto패키지안에 테이블명Dto 생성완료.(tmr_file이면 TmrFileDto로 이름 생성함)

---
+++ tem3/src/dto/DocumentInfoDto.java
@@ -0,0 +1,87 @@
+package dto;
+
+import java.sql.Date;
+
+public class DocumentInfoDto { //기록물 세부정보 테이블
+	private int dcryNo;		//파일번호
+	private String sicId;	//회원id
+	private Date dcryDttm;	//발급일자
+	private String dcryEtc;	//비고
+	private int artrNo;		//의사번호
+	private String hosId;	//병원id
+	
+	
+	
+	//생성자
+	public DocumentInfoDto() {
+		super();
+	}
+
+	public DocumentInfoDto(int dcryNo, String sicId, Date dcryDttm, String dcryEtc, int artrNo, String hosId) {
+		super();
+		this.dcryNo = dcryNo;
+		this.sicId = sicId;
+		this.dcryDttm = dcryDttm;
+		this.dcryEtc = dcryEtc;
+		this.artrNo = artrNo;
+		this.hosId = hosId;
+	}
+
+	//get set
+	public int getDcryNo() {
+		return dcryNo;
+	}
+
+	public void setDcryNo(int dcryNo) {
+		this.dcryNo = dcryNo;
+	}
+
+	public String getSicId() {
+		return sicId;
+	}
+
+	public void setSicId(String sicId) {
+		this.sicId = sicId;
+	}
+
+	public Date getDcryDttm() {
+		return dcryDttm;
+	}
+
+	public void setDcryDttm(Date dcryDttm) {
+		this.dcryDttm = dcryDttm;
+	}
+
+	public String getDcryEtc() {
+		return dcryEtc;
+	}
+
+	public void setDcryEtc(String dcryEtc) {
+		this.dcryEtc = dcryEtc;
+	}
+
+	public int getArtrNo() {
+		return artrNo;
+	}
+
+	public void setArtrNo(int artrNo) {
+		this.artrNo = artrNo;
+	}
+
+	public String getHosId() {
+		return hosId;
+	}
+
+	public void setHosId(String hosId) {
+		this.hosId = hosId;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
