836c2d89267a7d7dcbf874e4f73169042249cef2
User <User@YD03-15>
2020-02-19 18:20:28 +0900

    dadqsda

--- tem3/src/kjr/HosmemberDAO.java
+++ tem3/src/kjr/HosmemberDAO.java
@@ -2,6 +2,7 @@ package kjr;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import lastdto.hosMemberDTO;
 import kty.DAO;
@@ -67,6 +68,48 @@ public class HosmemberDAO extends DAO {
 		close();
 		return list;
 	}
-	
+	public ArrayList<hosMemberDTO> select(List<String> hosId){ //병원이름으로 조회
+		ArrayList<hosMemberDTO> list = new ArrayList<>();
+		sql="SELECT * FROM hos_member ";
+		int len = hosId.size();
+		sql += " where hos_id in( ";
+		for(int i=0;i>len;i++) {
+			if(i==len-1) {
+				sql += "?)";
+			}else {
+				sql += "?,";
+			}
+		}
+		
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			if(hosId!=null) {
+				for(int i=0;i>len;i++) {
+				pstmt.setString(i, hosId.get(i));
+				}
+			}
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				hosMemberDTO dto = new hosMemberDTO();
+				dto.setHosId(rs.getString("HOS_ID"));
+				dto.setHosBizno(rs.getString("HOS_BIZNO"));
+				dto.setHosName(rs.getString("HOS_NAME"));
+				dto.setHosPhone(rs.getString("HOS_PHONE"));
+				dto.setHosPw(rs.getString("HOS_PW"));
+				dto.setHosAddr(rs.getString("HOS_ADDR"));
+				dto.setHosLat(rs.getInt("HOS_LAT"));
+				dto.setHosLng(rs.getInt("HOS_LNG"));
+				dto.setHosBizTime(rs.getString("BIZ_TIME"));
+				list.add(dto);		
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+		close();
+		return list;
+	}
 	
 }
--- tem3/src/kjr/cmd/SearchListCMD.java
+++ tem3/src/kjr/cmd/SearchListCMD.java
@@ -2,6 +2,7 @@ package kjr.cmd;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import command.Command;
 import lastdto.hosMemberDTO;
 import kjr.HosmemberDAO;
+import kjr.SearchDAO;
 
 public class SearchListCMD implements Command {
 
@@ -17,19 +19,35 @@ public class SearchListCMD implements Command {
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String path ="aView/kjr/search_list.jsp"; //S7 검색 결과 리스트 페이지
-		////////////////////////////////////////////////////////////////////////////병원이름검색
-		String hosName; 	//검색한 병원이름 
-		hosName=request.getParameter("hosName");
-		System.out.println("병원명 = "+hosName);
-		HosmemberDAO dao = new HosmemberDAO();
+		////////////////////////////////////////////////////////////////////////////넘어오는 값 확인
+		String hosName; 	//검색한 병원이름
+		String[] cateCode;	//카테고리 코드
+		String areaCode; 		//지역(지하철)코드
 		ArrayList<hosMemberDTO> hosList = new ArrayList<>();
-		hosList = dao.select(hosName); //병원명으로 검색한 병원리스트 db에서 가져옴
+		HosmemberDAO hosDAO = new HosmemberDAO();
+		SearchDAO keywdDAO = new SearchDAO();
+		hosName=request.getParameter("hosName");
+		areaCode= request.getParameter("areaCode");
+		cateCode= request.getParameterValues("cateCode");
+		System.out.println("병원명 "+hosName);
+		System.out.println("지역코드"+areaCode);
+		if(cateCode!=null){
+			for(String cd : cateCode) {
+				System.out.println("카테고리"+cd);
+			}
+		}
+		
+		if(hosName!=null) { //병원이름으로 검색했을경우
+			hosList = hosDAO.select(hosName); //병원이름으로 병원정보 검색
+		}else {				//태그(지역,지하철,카테고리)로 검색했을경우
+			if(areaCode!=null && cateCode!=null) { 
+				hosList = hosDAO.select();	//지역,카테고리 코드가 안넘어온경우 전체검색
+			}else {
+				hosList = keywdDAO.select(cateCode,areaCode); //지역, 카테고리 코드로 병원 정보를 가져옴 
+			}
+		}	
+		
 		request.setAttribute("hosList",hosList);
-		
-		///////////////////////////////////////////////////////////////////////////키워드로검색
-		String[] shcode= request.getParameterValues("shcode");
-		
-		
 		return path;
 	}
 
