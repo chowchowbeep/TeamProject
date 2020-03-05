9f8f423d596d09101c74b1812a985fb60a4ebf30
com <com@com-PC>
2020-03-05 17:30:34 +0900

    ajax추가..

--- tem3/src/command/Contoller.java
+++ tem3/src/command/Contoller.java
@@ -87,6 +87,7 @@ import cmd.ShospitalInfoForTestCMD;
 import cmd.SsearchMainCMD;
 import cmd.TestCMD;
 import kjr.ajax.CategoriAjaxCMD;
+import kjr.ajax.HosSttAjaxCMD;
 import kjr.ajax.LocaseachAjaxCMD;
 import kjr.ajax.ReviewDeleteAjaxCMD;
 import kjr.ajax.ReviewInsrtAjaxCMD;
@@ -266,7 +267,7 @@ public class Contoller extends HttpServlet {
 		 * cont.put("/ajax/UpdateUsers.do", new UpdateUsers()); cont.put("/Upload.do",
 		 * new FileUpload()); cont.put("/GetDeptCnt.do", new GetDeptCnt());
 		 */
-		
+		cont.put("/ajax/HosSttAjaxCMD.do", new HosSttAjaxCMD()); // 지역코드 가져오는 ajax
 		cont.put("/ajax/ReviewDeleteAjaxCMD.do", new ReviewDeleteAjaxCMD()); // 지역코드 가져오는 ajax
 		cont.put("/ajax/LocaseachAjaxCMD.do", new LocaseachAjaxCMD()); // 지역코드 가져오는 ajax
 		cont.put("/ajax/CategoriAjaxCMD.do", new CategoriAjaxCMD()); // 카테고리코드 가져오는 ajax
