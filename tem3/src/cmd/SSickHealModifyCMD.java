5068d689b16398f44a0fbbe4b13d7b3d9ed05b3a
Da yeon <ekdus5530@naver.com>
2020-03-24 14:17:32 +0900

    200324 건강정보 수정 작성중.....

--- tem3/src/cmd/SSickHealModifyCMD.java
+++ tem3/src/cmd/SSickHealModifyCMD.java
@@ -7,14 +7,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import command.Command;
+import lastdto.healthInfoDTO;
+import leedy.sickHealthModifyDAO;
 
 public class SSickHealModifyCMD implements Command {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		//S19 건강정보 수정 페이지
-		String path ="aView/leedy/sick_healModify.jsp"; //
+		
+		String id = (String)request.getSession().getAttribute("sic_id");
+		
+		healthInfoDTO dto;
+		sickHealthModifyDAO dao = new sickHealthModifyDAO();
+		
+		
+	
+		
+		String path ="aView/leedy/sick_healModify.jsp"; //건강정보 수정 페이지
 		
 		return path;
 	}
